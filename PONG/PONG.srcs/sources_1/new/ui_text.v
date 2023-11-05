c3f8298e14c545a1c1cda11f7e1cb55f372f8e9a
Kittitat Tuntisak <58716471+IamKittitat@users.noreply.github.com>
2023-11-05 18:11:14 +0700

    feat: setup project

---
+++ PONG/PONG.srcs/sources_1/new/ui_text.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/05/2023 06:09:48 PM
+// Design Name: 
+// Module Name: ui_text
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ui_text(
+
+    );
+endmodule
